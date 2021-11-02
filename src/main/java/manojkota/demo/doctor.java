89e6a14f9187a9533ec9355e8a90e86cb0637bef
manoj kota <kota.manoj8@gmail.com>
2021-11-02 12:07:23 -0500

    ig

---
+++ src/main/java/manojkota/demo/doctor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manojkota.demo;
+
+/**
+ *
+ * @author S545400
+ */
+public class doctor {
+    
+}
---
+++ src/test/java/manojkota/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package manojkota.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
